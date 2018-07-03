6fff1316fbf24c6658b4e35b051fd072c7d06b74
lishanhui <lsh@ascend-tech.com.cn>
2018-07-03 09:17:50 +0800

    添加包名

---
+++ app/src/main/java/com/ascend/assetcheck_jinhua/result/LoginResultBack.java
@@ -0,0 +1,13 @@
+package com.ascend.assetcheck_jinhua.result;
+
+import com.ascend.assetcheck_jinhua.api.BaseResult;
+
+/**
+ * 作者：lishanhui on 2018-07-03.
+ * 描述：登录返回
+ */
+
+public class LoginResultBack extends BaseResult {
+
+
+}
